d4d98fd34274f8e4409cb3c2ec468d3d8c79004e
memed <rafik_ahmed@outlook.fr>
2020-04-23 20:20:08 +0200

    add BottleBill Table

--- database/init/01_init.sql
+++ database/init/01_init.sql
@@ -1,16 +1,26 @@
 create table userfca (
-  user_id        bigint PRIMARY KEY,
-  login          varchar(50) not null,
-  email          varchar(100) not null,
-  pass           varchar(100) not null,
-  firstname      varchar(100),
-  lastname       varchar(100),
-  accesstoken    varchar(50)
+  user_id           bigint PRIMARY KEY,
+  login             varchar(50) not null,
+  email             varchar(100) not null,
+  pass              varchar(100) not null,
+  firstname         varchar(100),
+  lastname          varchar(100),
+  accesstoken       varchar(50)
 );
 
 create table bill(
-  bill_id        serial primary key,
-  amount        double precision ,
-  date          date,
-  payer         integer references userfca (user_id)
+  bill_id           serial primary key,
+  amount            double precision ,
+  date              date,
+  payer             integer references userfca (user_id)
+);
+
+create type bottletype as enum ('Pastis', 'Get', 'Vodka', 'Rhum');
+
+create table bottlebill(
+  bottle_bill_id    serial primary key,
+  quantity          integer,
+  date              date,
+  bottle_type       bottletype,
+  giver             integer references userfca (user_id)
 );
