8550506d4890b43ac918a4d5c5a79cf62b298184
memed <rafik_ahmed@outlook.fr>
2020-04-26 20:29:30 +0200

    add picture model

---
+++ rs-api/src/main/kotlin/org/fca/rsapi/model/Picture.kt
@@ -0,0 +1,26 @@
+package org.fca.rsapi.model
+
+import com.fasterxml.jackson.annotation.JsonBackReference
+import java.time.temporal.TemporalAmount
+import java.util.*
+import javax.persistence.*
+
+@Entity
+data class Picture (
+
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val pictureId: Long?,
+
+        val name: String,
+
+        val path: String,
+
+        @ManyToOne(fetch = FetchType.EAGER)
+        @JoinColumn(name = "album")
+        @JsonBackReference
+        var album: Album? = null
+) {
+        override fun toString(): String {
+                return "Picture(pictureId=$pictureId, name='$name', path='$path', album=$album)"
+        }
+}
