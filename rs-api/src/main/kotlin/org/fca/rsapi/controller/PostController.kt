2edfab09bb78db95315c1efbf88af5edc90047d7
memed <rafik_ahmed@outlook.fr>
2020-04-28 00:09:10 +0200

    add post controller

---
+++ rs-api/src/main/kotlin/org/fca/rsapi/controller/PostController.kt
@@ -0,0 +1,69 @@
+package org.fca.rsapi.controller
+
+import org.fca.rsapi.dto.PostDTO
+import org.fca.rsapi.model.Post
+import org.fca.rsapi.repository.PostRepository
+import org.fca.rsapi.service.PostService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/post")
+class PostController (private val postRepository: PostRepository, private val albumService: PostService) {
+
+    @GetMapping("/")
+    fun getAll(): List<Post> =
+            postRepository.findAll()
+
+
+    @PostMapping("/")
+    fun createNew(@Valid @RequestBody dto: PostDTO): ResponseEntity<Post> {
+        return ResponseEntity.ok(albumService.save(dto))
+    }
+
+
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable(value = "id") id: Long): ResponseEntity<Post> {
+        return postRepository.findById(id).map { post ->
+            ResponseEntity.ok(post)
+        }.orElse(ResponseEntity.notFound().build())
+
+    }
+
+    @PutMapping("/{id}")
+    fun updateById(@PathVariable(value = "id") id: Long,
+                   @Valid @RequestBody new: Post): ResponseEntity<Post> {
+
+        return postRepository.findById(id).map { existing ->
+            val updated: Post = existing
+                    .copy(title = new.title, body = new.body, date = new.date, writer = new.writer)
+            ResponseEntity.ok().body(postRepository.save(updated))
+        }.orElse(ResponseEntity.notFound().build())
+
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable(value = "id") id: Long): ResponseEntity<Void> {
+        return postRepository.findById(id).map { post  ->
+            postRepository.delete(post)
+            ResponseEntity<Void>(HttpStatus.OK)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @GetMapping("/user/{id}")
+    fun getByUserId(@PathVariable(value = "id") id: Long): ResponseEntity<List<Post>> {
+        val res = albumService.getByUserId(id)
+        return if (res.isEmpty()){
+            ResponseEntity<List<Post>>(HttpStatus.NOT_FOUND)
+        }
+        else{
+            ResponseEntity.ok(res)
+        }
+    }
+
+
+    
+}
