f0fa4fe7fde5de69235c594da62e3de903fbe846
memed <rafik_ahmed@outlook.fr>
2020-04-27 16:44:56 +0200

    add post mapper

---
+++ rs-api/src/main/kotlin/org/fca/rsapi/mapper/PostMapper.kt
@@ -0,0 +1,27 @@
+package org.fca.rsapi.mapper
+
+import org.fca.rsapi.dto.PostDTO
+import org.fca.rsapi.model.Post
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class PostMapper {
+    companion object {
+        fun mapper(dto: PostDTO): Post{
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH)
+            val date = Date.from(LocalDate.parse(dto.date, formatter).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())
+            return Post(dto.postId, dto.title, dto.body, date)
+        }
+
+        fun mapper(post: Post): PostDTO {
+            val date = SimpleDateFormat("dd/MM/yyyy").format(post.date)
+            return PostDTO(post.postId, post.title,post.body, date, post.writer?.userId)
+
+        }
+    }
+
+
+}
